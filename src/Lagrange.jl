5f13b46a779ba3ed8a24992dcdbc749d40c4d5b1
Cook, James <james.cook@ukaea.uk>
2022-04-21 16:21:52 +0100

    remove a, b and replace with -1, 1 as Lagrange is always in reference cell

--- src/Lagrange.jl
+++ src/Lagrange.jl
@@ -123,14 +123,14 @@ function lagrange(x, nodes::NDimNodes, dofs)
   return output
 end
 
-maybesolvedofs!(dofs, nodes, a, b, _::DelayDofsSolve) = nothing
-function maybesolvedofs!(dofs, nodes, a, b, _::SolveDofsNow)
-  M = lumassmatrix(nodes, a, b)
+maybesolvedofs!(dofs, nodes, _::DelayDofsSolve) = nothing
+function maybesolvedofs!(dofs, nodes, _::SolveDofsNow)
+  M = lumassmatrix(nodes)
   ldiv!((@view dofs[:]), M, (@view dofs[:]))
 end
 
-function solvedofsifnecessary!(dofs, nodes, a, b, d::DelayDofsSolve)
-  maybesolvedofs!(dofs, nodes, a, b, opposite(d))
+function solvedofsifnecessary!(dofs, nodes, d::DelayDofsSolve)
+  maybesolvedofs!(dofs, nodes, opposite(d))
 end
 
 function lagrangeinner!(dofs, x::AbstractVector, nodes, value)
@@ -139,65 +139,62 @@ function lagrangeinner!(dofs, x::AbstractVector, nodes, value)
   end
 end
 function lagrange!(dofs, x::AbstractVector, nodes::NDimNodes, value,
-    a=_a(nodes), b=_b(nodes), solvedofsornot::MaybeSolveDofs=SolveDofsNow())
+    solvedofsornot::MaybeSolveDofs=SolveDofsNow())
   @assert size(dofs) == Tuple(length(n) for n in nodes)
   lagrangeinner!(dofs, x, nodes, value)
-  maybesolvedofs!(dofs, nodes, a, b, solvedofsornot)
+  maybesolvedofs!(dofs, nodes, solvedofsornot)
 end
 function lagrange!(dofs, xs::AbstractArray{<:Real, 2}, nodes::NDimNodes, value::T,
-    a=_a(nodes), b=_b(nodes), solvedofsornot::MaybeSolveDofs=SolveDofsNow()) where {T}
+    solvedofsornot::MaybeSolveDofs=SolveDofsNow()) where {T}
   @assert size(dofs) == Tuple(length(n) for n in nodes)
-  @assert size(xs, 1) == length(a)
   for i in 1:size(xs, 2)
     val = (T <: Number) ? value : value[i] # this isn't good but hopefully the compiler is cleverer than me
     lagrangeinner!(dofs, (@view xs[:, i]), nodes, val)
   end
-  maybesolvedofs!(dofs, nodes, a, b, solvedofsornot)
+  maybesolvedofs!(dofs, nodes, solvedofsornot)
 end
 
 quadrature(::Val{1}) = QuadGK.quadgk
 quadrature(::Val{N}) where N = HCubature.hcubature
 quadrature(n::NDimNodes) = quadrature(Val(ndims(n)))
-function lagrange!(dofs, nodes::NDimNodes, f::F, a=_a(nodes), b=_b(nodes),
+function lagrange!(dofs, nodes::NDimNodes, f::F,
     solvedofsornot::MaybeSolveDofs=SolveDofsNow()) where {F<:Function}
   quad = quadrature(nodes)
+  a, b = _a(nodes), _b(nodes)
   @assert size(dofs) == Tuple(length(n) for n in nodes)
   for i in CartesianIndices(dofs)
     t = Tuple(i)
     dofs[i] = quad(x->f(x) * lagrange(x, nodes, t, 1), a, b, atol=10eps(), rtol=sqrt(eps()))[1]
   end
-  maybesolvedofs!(dofs, nodes, a, b, solvedofsornot)
+  maybesolvedofs!(dofs, nodes, solvedofsornot)
 end
-@memoize function massmatrix(nodesi::T, nodesj::U, a=_a(nodesi), b=_b(nodesi)
+function massmatrix(nodesi::T, nodesj::U,
     ) where {T<:AbstractLagrangeNodes,U<:AbstractLagrangeNodes}
-  @warn "Using non-orthogonal nodes $T, $U"
   output = zeros(length(nodesi), length(nodesj))
   for j in eachindex(nodesj), i in eachindex(nodesi)
-    output[i, j] = QuadGK.quadgk(x->lagrange(x, nodesi, i, 1) * lagrange(x, nodesj, j, 1), a, b)[1]
+    output[i, j] = QuadGK.quadgk(x->lagrange(x, nodesi, i, 1) * lagrange(x, nodesj, j, 1), -1, 1)[1]
   end
   return output
 end
-function massmatrix(nodesi::T, nodesj::T, a=_a(nodesi), b=_b(nodesi)
+function massmatrix(nodesi::T, nodesj::T
     ) where {U, T<:AbstractOrthogLagrangeNodes{U}}
-#  @info "Calling memoized massmatrix(nodesi::T, nodesj::T, a=_a(nodesi), b=_b(nodesi))"
   return Diagonal(deepcopy(weights(nodesi)))
 end
 
-massmatrix(nodes::AbstractLagrangeNodes, a=_a(nodes), b=_b(nodes)) = massmatrix(nodes, nodes, a, b)
+massmatrix(nodes::AbstractLagrangeNodes) = massmatrix(nodes, nodes)
 
 const massmatrixdictsdict = Dict{UInt64, Any}()
 
-function massmatrixdictionary(nodesi::NDimNodes, nodesj::NDimNodes,
-    a=_a(nodesi), b=_b(nodesi))
-  key = mapreduce(hash, hash, (nodesi, nodesj, a, b))
+function massmatrixdictionary(nodesi::NDimNodes, nodesj::NDimNodes)
+  key = mapreduce(hash, hash, (nodesi, nodesj))
   haskey(massmatrixdictsdict, key) && return massmatrixdictsdict[key]
 #  throw(ErrorException("just show me the stack trace"))
-  @info "Calling memoized massmatrixdictionary(...), $a, $b"
-  @assert length(nodesi) == length(nodesj) == length(a) == length(b)
+  @info "Calling memoized massmatrixdictionary(...)"
+  @assert length(nodesi) == length(nodesj)
   szi = Tuple(length(n) for n in nodesi)
   szj = Tuple(length(n) for n in nodesj)
   M = zeros(prod(szi), prod(szj))
-  Ms = [massmatrix(nodesi[i], nodesj[i], a[i], b[i]) for i in eachindex(nodesi)]
+  Ms = [massmatrix(nodesi[i], nodesj[i]) for i in eachindex(nodesi)]
   @assert length(Ms) == length(nodesi)
   d = Dict()
   for i in CartesianIndices(szi), j in CartesianIndices(szj)
@@ -208,25 +205,39 @@ function massmatrixdictionary(nodesi::NDimNodes, nodesj::NDimNodes,
   massmatrixdictsdict[key] = d
   return d
 end
-function massmatrixdictionary(nodes::NDimNodes, a=_a(nodes), b=_b(nodes))
-  return massmatrixdictionary(nodes, nodes, a, b)
-end
+massmatrixdictionary(nodes::NDimNodes) = massmatrixdictionary(nodes, nodes)
 
+const _massmatrixdict = Dict{UInt64, Any}()
 
-function massmatrix(nodesi::NDimNodes, nodesj::NDimNodes, a=_a(nodesi), b=_b(nodesi))
+function massmatrix(nodesi::NDimNodes, nodesj::NDimNodes)
+  key = mapreduce(hash, hash, (nodesi, nodesj))
+  haskey(_massmatrixdict, key) && return _massmatrixdict[key]
+
   szi = Tuple(length(n) for n in nodesi)
   szj = Tuple(length(n) for n in nodesj)
   M = zeros(prod(szi), prod(szj))
-  d = massmatrixdictionary(nodesi, nodesj, a, b)
+  d = massmatrixdictionary(nodesi, nodesj)
   for (j, jj) in enumerate(CartesianIndices(szj)), (i, ii) in enumerate(CartesianIndices(szi))
     indsi = Tuple(ii)
     indsj = Tuple(jj)
     M[i, j] = d[(indsi, indsj)]
   end
-  return M
+  _massmatrixdict[key] = M
+  return _massmatrixdict[key]
 end
-massmatrix(nodes::NDimNodes, a=_a(nodes), b=_b(nodes)) = massmatrix(nodes, nodes, a, b)
-lumassmatrix(args...) = lu(massmatrix(args...))
+
+massmatrix(nodes::NDimNodes) = massmatrix(nodes, nodes)
+
+const _lumassmatrixdict = Dict{UInt64, Any}()
+function lumassmatrix(args...)
+  key = mapreduce(hash, hash, args)
+  #haskey(_lumassmatrixdict, key) && return _lumassmatrixdict[key]
+
+  output = lu(massmatrix(args...))
+  #_lumassmatrixdict[key] = output
+  return output
+end
+
 
 
 const surfacefluxstiffnessmatrixdict = Dict{UInt64, Any}()
@@ -320,10 +331,10 @@ end
 
 
 
-function convertbases!(dofsto, dofsfrom, nodesfrom, nodesto, a=_a(nodesfrom), b=_b(nodesfrom))
+function convertbases!(dofsto, dofsfrom, nodesfrom, nodesto)
   @assert size(dofsto) == size(dofsfrom)
-  Mtoto = massmatrix(nodesto, nodesto, a, b)
-  Mtofrom = massmatrix(nodesto, nodesfrom, a, b)
+  Mtoto = massmatrix(nodesto, nodesto)
+  Mtofrom = massmatrix(nodesto, nodesfrom)
   @views dofsto[:] .= Mtoto \ Mtofrom * dofsfrom[:]
 end
 
