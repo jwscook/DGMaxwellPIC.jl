c25a1136d4c8f20e00d0124dd2e52a2d115c730c
Cook, James <james.cook@ukaea.uk>
2022-05-17 13:48:43 +0100

    refactor away state

--- src/Cells.jl
+++ src/Cells.jl
@@ -30,8 +30,8 @@ end
 state(c::Cell) = c.state
 dofs(c::Cell) = vcat(((@view c.state.q[i][:]) for i in 1:6)...) # TODO think about shape
 currentdofs(c::Cell) = vcat(((@view c.state.q[i][:]) for i in 7:9)...) # TODO think about shape
-chargedofs(c::Cell) = @view c.state.q[10]
-workdofs(c::Cell) = @view c.state.q[11]
+chargedofs(c::Cell) = dofs(c.state.q, 10)
+workdofs(c::Cell) = dofs(c.state.q, 11)
 referencex(x, c::Cell{N}) where {N} = @. ((x - c.lower) * c.inverselengths * 2 - 1)
 originalx(x, c::Cell{N}) where {N} = @. ((x + 1) /2 * (c.upper - c.lower) + c.lower)
 referencex!(x, c::Cell{N}) where {N} = (@. x = (x - c.lower) * c.inverselengths * 2 - 1)
--- src/States.jl
+++ src/States.jl
@@ -2,20 +2,27 @@
 # in N-dimensionsal space
 struct State{N, T<:BasisFunctionType}
   q::Vector{Array{Float64, N}}
+  dofshape::NTuple{N,Int}
   nodes::NDimNodes
   function State(ncoeffsperdim, ::Type{T}) where {T<:BasisFunctionType}
     N = length(ncoeffsperdim)
     vecofdofs = [zeros(ncoeffsperdim...) for i in 1:11]
     nodes = NDimNodes(ncoeffsperdim, T)
-    return new{N, T}(vecofdofs, nodes) # need only be 6?
+    return new{N, T}(vecofdofs, Tuple(ncoeffsperdim), nodes) # need only be 6?
   end
 end
-Base.size(s::State) = size(s.q[1])
-dofshape(s::State) = size(s.q[1])
+Base.size(s::State) = s.dofshape
+dofshape(s::State) = s.dofshape
+numcomponents(s::State) = 11
 zero!(s::State) = fill!.(s.q, 0)
 state(s::State) = s
 nodes(s::State) = s.nodes
 workdofs(s::State) = s.q[11]
+
+function dofs(s::State, component::Integer)
+  return s.q[component]
+end
+
 
 function dofs!(s::State, x::Number)
   for i in 1:6
@@ -24,12 +31,31 @@ function dofs!(s::State, x::Number)
   return s
 end
 
-function dofs!(s::State, x)
+function dofs!(s::State, x, component::Integer)
+  sqi = s.q[component]
+  for j in LinearIndices(dofshape(s))
+    sqi[j] = x[j]
+  end
+  return s
+end
+
+function dofs!(s::State, x, components::UnitRange{Int}=1:6)
   l = prod(dofshape(s))
-  for i in 1:6
+  for i in components
     sqi = s.q[i]
     for j in LinearIndices(dofshape(s))
       sqi[j] = x[(i-1)*l + j]
+    end
+  end
+  return s
+end
+
+function incrementdofs!(s::State, x, components::UnitRange{Int}=1:6)
+  l = prod(dofshape(s))
+  for i in components
+    sqi = s.q[i]
+    for j in LinearIndices(dofshape(s))
+      sqi[j] += x[(i-1)*l + j]
     end
   end
   return s
